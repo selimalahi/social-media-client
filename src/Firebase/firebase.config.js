28d915e61a963a008746bc6a1c40d51e268b8550
selimalahi <alahi.selim@gmail.com>
2022-12-30 22:55:47 +0600

    social-media update

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9sDevqjwuyXvc9d8iWaP4s_33oTiChpI",
+  authDomain: "social-media-34bc2.firebaseapp.com",
+  projectId: "social-media-34bc2",
+  storageBucket: "social-media-34bc2.appspot.com",
+  messagingSenderId: "1019400499670",
+  appId: "1:1019400499670:web:cd0bf7ddebde5db32e3477"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Pages/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Comments from './../Comment/Comments';
+
+const About = () => {
+    return (
+        <div>
+            <div class="container">
+                <div class="card-header text-center">
+                   <h1> Name: Md. Selim Alahi</h1>
+                   <h2>Email: alahi.selim@gmail.Comments</h2>
+                   <h3>University: Eastern University </h3>
+                   <p> Address: House No. 86, Rd No 5, Dhaka Uddan, Dhaka - 1216, Bangladesh </p>
+
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default About;
